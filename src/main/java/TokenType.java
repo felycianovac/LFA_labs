13273b48405ed2a626e489e8ffd3bfaf67767f0b
felycianovac <felicianovac4@gmail.com>
2024-04-27 00:14:56 +0300

    Implement Parser and AST build add enum TokenType

---
+++ src/main/java/TokenType.java
@@ -0,0 +1,18 @@
+public enum TokenType {
+
+    START_COMMAND,
+    PIPE_LINE,
+    NUMBER,
+    IMAGE_IDENTIFIER,
+    EQUALS,
+    FILE_PATH,
+    FOLDER_PATH,
+    IMAGE_TYPE,
+    PARAMETER,
+    COMMAND,
+    QUOTE,
+    UNKNOWN
+
+
+
+}
